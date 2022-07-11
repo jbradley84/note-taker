9e08cf4e8fa35d8277162db3b21f720ef6d67e70
jbradley84 <utbradley84@yahoo.com>
2022-07-11 13:26:44 -0500

    wrote function to add new notes to db.json

--- server.js
+++ server.js
@@ -1,11 +1,62 @@
+const fs = require('fs');
+const path = require('path');
 const { notes } = require('./db/db.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// MIDDLEWARE
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
+// NOTES FUNCTIONS
+function findById(id, notesArray) {
+   const result = notesArray.filter(note => note.id === id)[0];
+   return result;
+}
+
+function createNewNote(body, notesArray) {
+   const note = body;
+   notesArray.push(note);
+   fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+   );
+
+   return note;
+}
+
+
+// API ROUTES
+// get request to RETREIVE NOTES
 app.get('/api/notes', (req, res) => {
    res.json(notes);
 });
+
+// get request to RETRIEVE SINGLE NOTE by ID
+app.get('/api/notes/:id', (req, res) => {
+   const result = findById(req.params.id, notes);
+   if (result) {
+      res.json(result);
+   } else {
+      res.send(404);
+   }
+})
+
+// post request to ADD NEW NOTE
+app.post('/api/notes', (req, res) => {
+   // set id based on what the next index of the array will be
+   req.body.id = notes.length.toString();
+
+   // add note to json file and notes array in this function
+   const note = createNewNote(req.body, notes);
+
+   res.json(note);
+});
+
 
 app.listen(PORT, () => {
    console.log(`API server now on port ${PORT}!`);
